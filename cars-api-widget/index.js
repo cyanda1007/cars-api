737f0d459128903435d338d723e2d64d56625b0e
cyanda1007 <siyaphiwa196@gmail.com>
2023-06-04 15:05:08 +0200

    added files

--- cars-api-widget/index.js
+++ cars-api-widget/index.js
@@ -1,30 +1,23 @@
-const vehicleTemplate = document.querySelector(".vehicleTemplate");
-const TemplateForUsers = Handlebars.compile(vehicleTemplate.innerText);
-
-const elemCars = document.querySelector(".cars");
-const elemMake = document.querySelector(".make");
-const elemColor = document.querySelector(".color");
-
-axios.get("http://api-tutor.herokuapp.com/v1/cars").then((result) => {
+const vehicleTemplateText = document.querySelector(".vehicleTemplate");
+const userTemplate = Handlebars.compile(vehicleTemplateText.innerText);
+const colorsTemplateSource = document.querySelector(".colorsTemplate");
+const colorsTemplate = Handlebars.compile(colorsTemplateSource.innerHTML);
+const brandsTemplateSource = document.querySelector(".brandsTemplate");
+const brandsTemplate = Handlebars.compile(brandsTemplateSource.innerHTML);
+const carsElem = document.querySelector(".cars");
+const makeElem = document.querySelector(".make");
+const colorElem = document.querySelector(".colors");
+axios.get("https://api-tutor.herokuapp.com/v1/cars").then((result) => {
   const cars = result.data;
-
-  elemCars.innerHTML = TemplateForUsers({
-    cars,
-  });
+  carsElem.innerHTML = userTemplate({ cars });
 });
-
-axios.get("http://api-tutor.herokuapp.com/v1/colors").then((result) => {
-  const color = result.data;
-
-  elemColor.innerHTML = TemplateForUsers({
-    color,
+axios
+  .get("https://api-tutor.herokuapp.com/v1/colors")
+  .then(function (response) {
+    const colorsElem = document.querySelector(".colors");
+    colorsElem.innerHTML = colorsTemplate({ colors: response.data });
   });
-});
-
-axios.get("http://api-tutor.herokuapp.com/v1/make").then((result) => {
-  const brand = result.data;
-
-  elemMake.innerHTML = TemplateForUsers({
-    brand,
-  });
+axios.get("https://api-tutor.herokuapp.com/v1/makes").then(function (response) {
+  const brandsElem = document.querySelector(".brands");
+  brandsElem.innerHTML = brandsTemplate({ brands: response.data });
 });
